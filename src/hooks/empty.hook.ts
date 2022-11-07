a9dd78653bac0022af22ce16691ddd1d6a4773ad
henryk <henryk.daneluz@wedigital.it>
2022-11-07 18:13:56 +0100

    improved commets

--- src/hooks/empty.hook.ts
+++ src/hooks/empty.hook.ts
@@ -3,6 +3,15 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 /**
  * TODO: file should exports in format 'hook': function()
  * eg: 'onRequest': onRequestHook , 'onPreValidation': onPreValidationHook , etc...
+ * Available Hooks:
+ * onRequest, onResponse, preParsing, preValidation, preHandler, preSerialization, onSend, onTimeout, onError
+ * 
+ * // // Example with an array. All hooks support this syntax.
+ * //
+ * // preHandler: [function (request, reply, done) {
+ * //   // This hook will always be executed after the shared `preHandler` hooks
+ * //   done()
+ * // }],
  */
 
 const emptyHook = (req: FastifyRequest, reply: FastifyReply, done: any) => {
